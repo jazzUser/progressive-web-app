36c933f6c8c72c5e6ad6b7718cd9b95236b50a98
Jasmina Muharemovic <jasminamhrmvc@proton.me>
2024-10-24 16:55:55 +0200

    Add dashboard, header and main content

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-// Register the service worker
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
-            .then((registration) => {
-                console.log('Service Worker registered with scope:', registration.scope);
-            })
-            .catch((error) => {
-                console.log('Service Worker registration failed:', error);
-            });
-    });
-}
+document.getElementById("toggleSidebar").addEventListener("click", () => {
+  document.getElementById("sidebar").classList.toggle("hidden");
+});
--- sw.js
+++ sw.js
@@ -1,40 +1,38 @@
 // Cache version
-const CACHE_NAME = 'pwa-example-cache-v1';
+const CACHE_NAME = "pwa-example-cache-v1";
 const urlsToCache = [
-  './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './icons/app-icon-192x192.png',  // Updated icon path
-  './icons/app-icon-512x512.png'    // Updated icon path
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./icons/app-icon-192x192.png", // Updated icon path
+  "./icons/app-icon-512x512.png", // Updated icon path
 ];
 
 // Install the service worker and cache the app shell
-self.addEventListener('install', (event) => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
 // Fetch cached assets when offline
-self.addEventListener('fetch', (event) => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then((response) => {
-        if (response) {
-          return response; // return the cached response
-        }
-        return fetch(event.request); // fetch from the network
-      })
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response; // return the cached response
+      }
+      return fetch(event.request); // fetch from the network
+    })
   );
 });
 
 // Activate service worker and remove old caches
-self.addEventListener('activate', (event) => {
+self.addEventListener("activate", (event) => {
   const cacheWhitelist = [CACHE_NAME];
   event.waitUntil(
     caches.keys().then((cacheNames) => {
